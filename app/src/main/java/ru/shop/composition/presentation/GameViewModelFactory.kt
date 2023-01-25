728dc24af7be274d4fb472a955c467b282d36878
Ilya Lisitskiy <ilyalisi7skiy@gmail.com>
2023-01-25 14:52:47 +0300

    Adding GameViewModelFactory

---
+++ app/src/main/java/ru/shop/composition/presentation/GameViewModelFactory.kt
@@ -0,0 +1,21 @@
+package ru.shop.composition.presentation
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.shop.composition.domain.entity.Level
+
+class GameViewModelFactory(
+    private val level: Level,
+    private val application: Application
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(GameViewModel::class.java)) {
+            return GameViewModel(application, level) as T
+        } else {
+            throw RuntimeException("Unknown view model class $modelClass")
+        }
+
+    }
+}
